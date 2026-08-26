6065a5d833d7f213c954d581e7d130932f6b6cda
Knappkin <44062671+Knappkin@users.noreply.github.com>
2026-08-25 21:32:03 -0400

    Hazards Work

--- Assets/Scripts/HazardSPawner.cs
+++ Assets/Scripts/HazardSPawner.cs
@@ -2,6 +2,15 @@ using UnityEngine;
 
 public class HazardSPawner : MonoBehaviour
 {
+
+    public Camera cam;
+    public int gridWidth;
+    public float tileSize;
+    private float halfTileSize;
+
+    public float baseSpawnRate;
+    public AnimationCurve varianceCurve;
+    public AnimationCurve intensityIncreaseCurve;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +20,7 @@ public class HazardSPawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector2 camPos = Camera.main.transform.position;
+        int camHeight = Mathf.RoundToInt(camPos.y);
     }
 }
