dfb2049bb0f25accd7c45d4c4218a392b3156984
ravin <hewabadd@sheridancollege.ca>
2026-09-22 14:31:12 -0400

    some changes

--- Assets/Scripts/Lobby/MainMenuManager.cs
+++ Assets/Scripts/Lobby/MainMenuManager.cs
@@ -10,27 +10,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public int lobbyCode;
+    public string joinCode;
+    private void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
     public void ClickedHost()
     {
-
+        joinCode = StartHostWithRelay(1, "udp").ToString();
     }
 
     public void ClickedJoin()
     {
-        
+        _ = StartClientWithRelay(joinCode, "udp");
     }
 
     public async Task<string> StartHostWithRelay(int maxConnections, string connectionType)
     {
         await UnityServices.InitializeAsync();
+        Debug.Log("Unity services initialized");
         if (!AuthenticationService.Instance.IsSignedIn)
         {
             await AuthenticationService.Instance.SignInAnonymouslyAsync();
         }
         var allocation = await RelayService.Instance.CreateAllocationAsync(maxConnections);
+        Debug.Log("allocation data set");
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, connectionType));
         var joinCode = await RelayService.Instance.GetJoinCodeAsync(allocation.AllocationId);
+        Debug.Log("join code gotted");
         return NetworkManager.Singleton.StartHost() ? joinCode : null;
     }
 
@@ -45,6 +52,15 @@ public class MainMenuManager : MonoBehaviour
         var allocation = await RelayService.Instance.JoinAllocationAsync(joinCode: joinCode);
         NetworkManager.Singleton.GetComponent<UnityTransport>().SetRelayServerData(AllocationUtils.ToRelayServerData(allocation, connectionType));
         return !string.IsNullOrEmpty(joinCode) && NetworkManager.Singleton.StartClient();
+    }
+
+    private void Update()
+    {
+        if (NetworkManager.Singleton.IsHost) Debug.Log("I am hosting");
+        if (NetworkManager.Singleton.IsClient) Debug.Log("I am clienting");
+        if (NetworkManager.Singleton.IsConnectedClient) Debug.Log("I am connected clienting?");
+        if (NetworkManager.Singleton.IsServer) Debug.Log("I am servering");
+        if (NetworkManager.Singleton.IsApproved) Debug.Log("I am approved?");
     }
 
 
